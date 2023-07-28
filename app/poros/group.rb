8b0d96210c9ab77a28f691de236c74898ca93b00
Andra Helton <andrahelton@gmail.com>
2023-07-27 20:49:55 -0600

    feat: Group and GroupList poros

---
+++ app/poros/group.rb
@@ -0,0 +1,29 @@
+class Group
+  attr_reader :id,
+              :host_id,
+              :host_name,
+              :name,
+              :friends,
+              :events
+
+  def initialize(info)
+    @id = info[:id]
+    @host_id = info[:attributes][:group_host_id]
+    @host_name = info[:attributes][:group_host_name]
+    @name = info[:attributes][:group_name]
+    @friends = add_friends(info[:attributes][:group_friends]) 
+    @events = add_events(info[:attributes][:group_events])
+  end
+
+  def add_friends(info)
+    if info
+      info.map { |friend| FriendList.new(friend) }
+    end
+  end
+
+  def add_events(info)
+    if info
+      info.map { |event| EventList.new(event) }
+    end
+  end
+end
