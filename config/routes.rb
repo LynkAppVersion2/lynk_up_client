5c820afe580283c56e740717ecd67dd9c10ab398
Andra Helton <andrahelton@gmail.com>
2023-08-15 11:56:49 -0600

    feat: User Dashboard menu - Events index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/users/:id/dashboard", to: "users#dashboard"
   get "/users/:user_id/events/:event_id", to: "events#show"
+  get "/users/:user_id/events", to: "events#index"
   get "/users/:user_id/events/new", to: "events#new"
+  get "/users/:user_id/groups", to: "groups#index"
+  get "/users/:user_id/friends", to: "friends#index"
 end
