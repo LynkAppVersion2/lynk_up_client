cf4d9b81fda321aa0b818c098f5a92d1fa79eec1
Andra Helton <andrahelton@gmail.com>
2023-08-15 21:53:03 -0600

    test: User Events Index - My Events and Invited Events in order of time, sorted past and upcoming

--- spec/features/events/index_spec.rb
+++ spec/features/events/index_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe "Events Index" do
   describe "As a logged in User" do
     let!(:user) { LynkUpFacade.new.find_user(1) }
-    let!(:user7) { LynkUpFacade.new.find_user(7) }
+    let!(:user_my_events_sorted) { LynkUpFacade.new.sort_events(user.my_events) }
+    let!(:user_invited_events_sorted) { LynkUpFacade.new.sort_events(user.invited_to_events) }
+    
+    let!(:user3) { LynkUpFacade.new.find_user(3) }
+    let!(:user3_invited_events_sorted) { LynkUpFacade.new.sort_events(user3.invited_to_events) }
+
+    let!(:user2) { LynkUpFacade.new.find_user(2) }
 
     describe "When I visit my Events Index", :vcr do
       before do
@@ -18,51 +24,115 @@ RSpec.describe "Events Index" do
         end
 
         describe "I see 'My Events'" do
-          context "For each event" do
-            it "I see the event info and name as a link to its show page" do
-              event = user.my_events[0]
-
-              within("#event-#{event.id}") do
-                expect(page).to have_link("Root - A medium length game")
-                expect(page).to have_content("9:00 PM")
-                expect(page).to have_content("08-04-24")
-
-                click_link "Root - A medium length game"
-                expect(current_path).to eq("/users/#{user.id}/events/#{event.id}")
-              end
-            end
-
-            it "If I have no events, I see 'No Events Created'" do
-              visit "/users/#{user7.id}/dashboard"
-              expect(page).to have_content("No Events Created")
-            end
-          end
-
           it "I see a button to create an event that takes me to a new page" do
             expect(page).to have_button("Create Event")
             click_button "Create Event"
             expect(current_path).to eq("/users/#{user.id}/events/new")
           end
+
+          describe "My Upcoming Events" do
+            before do
+              my_upcoming_events = LynkUpFacade.new.upcoming_events(user_my_events_sorted)
+              @event = my_upcoming_events[0]
+            end
+
+            context "For each event" do
+              it "I see the event info and name as a link to its show page" do
+                within("#event-#{@event.id}") do
+                  expect(page).to have_link("Going to the Movies")
+                  expect(page).to have_content("20:00:00")
+                  expect(page).to have_content("2023-11-02")
+  
+                  click_link "Going to the Movies"
+                  expect(current_path).to eq("/users/#{user.id}/events/#{@event.id}")
+                end
+              end
+  
+              it "If I have no events, I see 'No Events Created'" do
+                visit "/users/#{user2.id}/events/"
+                expect(page).to have_content("No Events Created")
+              end
+            end
+          end
         end
 
-        describe "I see 'Invited to' events" do
+        describe "My Past Events" do
+          before do
+            my_past_events = LynkUpFacade.new.past_events(user_my_events_sorted)
+            @event = my_past_events[0]
+          end
+
           context "For each event" do
             it "I see the event info and name as a link to its show page" do
-              event = user7.invited_to_events[1]
+              within("#event-#{@event.id}") do
+                expect(page).to have_link("Root")
+                expect(page).to have_content("20:00:00")
+                expect(page).to have_content("2023-06-03")
 
-              within("#event-#{event.id}") do
-                expect(page).to have_link("Denver Night Hang")
-                expect(page).to have_content("8:00 PM")
-                expect(page).to have_content("06-05-23")
-
-                click_link "Denver Night Hang"
-                expect(current_path).to eq("/users/#{user.id}/events/#{event.id}")
+                click_link "Root"
+                expect(current_path).to eq("/users/#{user.id}/events/#{@event.id}")
               end
             end
 
-            it "If I have no events, I see 'No Event Invitations'" do
-              visit "/users/#{user.id}/dashboard"
-              expect(page).to have_content("No Event Invitations")
+            it "If I have no events, I see 'No Events Created'" do
+              visit "/users/#{user2.id}/events/"
+              expect(page).to have_content("No Events Created")
+            end
+          end
+        end
+
+        describe "I see 'Invited to' events" do
+          before do
+            invited_upcoming_events = LynkUpFacade.new.upcoming_events(user3_invited_events_sorted)
+            @event = invited_upcoming_events[0]
+          end
+
+          describe "Upcoming Event Invitations" do
+            context "For each event" do
+              it "I see the event info and name as a link to its show page" do
+                visit "/users/#{user3.id}/events/"
+
+                within("#event-#{@event.id}") do
+                  expect(page).to have_link("Space Catan")
+                  expect(page).to have_content("20:00:00")
+                  expect(page).to have_content("2024-11-02")
+  
+                  click_link "Space Catan"
+                  expect(current_path).to eq("/users/#{user3.id}/events/#{@event.id}")
+                end
+              end
+  
+              it "If I have no events, I see 'No Event Invitations'" do
+                visit "/users/#{user2.id}/events/"
+                expect(page).to have_content("No Event Invitations")
+              end
+            end
+          end
+
+          describe "Past Event Invitations" do
+            before do
+              invited_past_events = LynkUpFacade.new.past_events(user3_invited_events_sorted)
+              @event = invited_past_events[0]
+            end
+
+            context "For each event" do
+              it "I see the event info and name as a link to its show page" do
+                visit "/users/#{user3.id}/events/"
+
+                within("#event-#{@event.id}") do
+                  expect(page).to have_link("Root")
+                  expect(page).to have_content("20:00:00")
+                  expect(page).to have_content("2023-06-03")
+  
+                  click_link "Root"
+                  expect(current_path).to eq("/users/#{user3.id}/events/#{@event.id}")
+                end
+              end
+  
+              it "If I have no events, I see 'No Event Invitations'" do
+                visit "/users/#{user2.id}/events/"
+                expect(page).to have_content("No Event Invitations")
+              end
             end
           end
         end
