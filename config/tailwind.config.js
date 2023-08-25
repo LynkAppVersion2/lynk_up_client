f6b2a3c3a95547a66e49e69e2eabb2526a1ce319
Andra Helton <andrahelton@gmail.com>
2023-08-25 14:29:40 -0600

    Add dashboard styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,18 +1,37 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+
 
 module.exports = {
   content: [
+    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*',
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
+
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      // sans: ['Graphik', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
+
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+        // 'background': '#E7DCDC',
+        'background': '#ddffa373',
+        // 'navigation': '#282030e3',
+        // 'navigation': '#202323',
+        'navigation': '#232626',
+        // 'white': '#ffffff',
+        // 'purple': '#3f3cbb',
     },
   },
+},
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
